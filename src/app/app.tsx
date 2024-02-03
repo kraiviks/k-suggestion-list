da8fc06656f2f3cb61d8cc0a867ebba4272700c6
Kraiviks <kraiviks@gmail.com>
2024-02-03 14:15:31 +0200

    add title

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -20,6 +20,7 @@ export function App(props: AppProps) {
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Suggestion list</title>
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
